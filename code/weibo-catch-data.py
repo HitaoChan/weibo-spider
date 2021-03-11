1167ce51d87f3b33d335d3e00e36bc740ae70633
HitaoChan <1147891620@qq.com>
2021-03-11 23:00:01 +0800

    爬取+存储

--- code/weibo-catch-data.py
+++ code/weibo-catch-data.py
@@ -2,22 +2,25 @@ import requests
 import pprint
 from bs4 import BeautifulSoup
 import re
-
+import time
+import time
 
 # pprint.pprint(response.json()) #格式化输出 只有接口爬虫能用
 
 
 # 接受json 提取键值内容
-def CatchData(url):
-    # 得到评论区
+def CatchData(id,max_id):
+    # 得到评论区 爬取第一页
     try:
+        time.sleep(0.25)
+        url="https://m.weibo.cn/comments/hotflow?id={}&mid={}&max_id_type=0".format(str(id),str(id))
         response = requests.get(url, timeout=30)
         # 如果不是200，引发HTTPEroor异常
         response.raise_for_status()
         response.encoding = response.apparent_encoding
-        data = response.json()
     except:
         print("产生异常")
+    data = response.json()
     cards = data['data']['data']
     for card in cards:
         # 加上一个楼中楼爬取
@@ -26,30 +29,36 @@ def CatchData(url):
             text = card.get('text')  # 评论
             like_count = card.get('like_count')  # 点赞
             user = card.get('user')  # 评论用户表信息
-            # cid = card.get('mid')
-            # url = url + "?cid=" + cid
-            # try:
-            #     response=requests.get(url,timeout=30)
-            #     response.raise_for_status()
-            #     response.encoding=response.apparent_encoding
-            #     data
+            max_id = card.get('max_id') #下一页的id
             name = user.get('screen_name')  # 用户名
             profile = user.get('profile_url')  # 用户主页
-            print("用户名:" + name, "评论:" + text, "点赞数量:" + str(like_count))
+            fileName=getTopic(id)
+            with open("../doc/"+fileName+".csv",mode='a',encoding='utf-8-sig') as f:
+                f.write(",".join([name,text,str(like_count)]))
+                f.write('\n')
+            print("catch one comment")
             # print(user)
             # print(name,text,like_count)
+    getComments(id,max_id) #开始爬取下一页
 
 
-def getTopic(id):
+def getComments(id,max_id):
+    # 如果不是最后一页就爬取下一页
+    if max_id != None:
+        #url=url+"&max_id={}&max_id_type=0".format(str(max_id))
+        CatchData(id,max_id)
+
+
+def getTopic(id: int)->str:
     # 打印话题热点
-    url = "https://m.weibo.cn/statuses/extend?id="+id
+    url = "https://m.weibo.cn/statuses/extend?id="+str(id)
     try:
         response = requests.get(url, timeout=30)
         # 如果不是200，引发HTTPEroor异常
         response.raise_for_status()
         response.encoding = response.apparent_encoding
         data = response.json()
-        pprint.pprint(data)
+        # pprint.pprint(data)
     except:
         print("产生异常")
     topic=data['data']
@@ -57,76 +66,20 @@ def getTopic(id):
     #print(type(text)) #text是str类型
     # 提取#开头结尾的的字符串
     pattern=re.compile(r'#.*#')
-    print(text)
-    str=pattern.search(text)
-    print(str.group(0))
+    # print(text)
+    str1=pattern.search(text)
+    #print(str1.group(0))
     # print(card.get('longTextContent'))
+    return str1.group(0)
 
 
-def start_crawl(cookie_dict,id):
-	base_url = 'https://m.weibo.cn/comments/hotflow?id={}&mid={}&max_id_type=0'
-	next_url = 'https://m.weibo.cn/comments/hotflow?id={}&mid={}&max_id={}&max_id_type={}'
-	page = 1
-	id_type = 0
-	comment_count = 0
-	requests_count = 1
-	res = requests.get(url=base_url.format(id,id), headers=headers,cookies=cookie_dict)
-	while True:
-		print('parse page {}'.format(page))
-		page += 1
-		try:
-			data = res.json()['data']
-			wdata = []
-			max_id = data['max_id']
-			for c in data['data']:
-				comment_count += 1
-				row = info_parser(c)
-				wdata.append(info_parser(c))
-				if c.get('comments', None):
-					temp = []
-					for cc in c.get('comments'):
-						temp.append(info_parser(cc))
-						wdata.append(info_parser(cc))
-						comment_count += 1
-					row['comments'] = temp
-				print(row)
-			with open('{}/{}.csv'.format(comment_path, id), mode='a+', encoding='utf-8-sig', newline='') as f:
-				writer = csv.writer(f)
-				for d in wdata:
-					writer.writerow([d['wid'],d['time'],d['text'],d['uid'],d['username'],d['following'],d['followed'],d['gender']])
-
-			time.sleep(5)
-		except:
-			print(res.text)
-			id_type += 1
-			print('评论总数: {}'.format(comment_count))
-
-		res = requests.get(url=next_url.format(id, id, max_id,id_type), headers=headers,cookies=cookie_dict)
-		requests_count += 1
-		if requests_count%50==0:
-			print(id_type)
-		print(res.status_code)
-
-
-def info_parser(data):
-	id,time,text =  data['id'],data['created_at'],data['text']
-	user = data['user']
-	uid,username,following,followed,gender = \
-		user['id'],user['screen_name'],user['follow_count'],user['followers_count'],user['gender']
-	return {
-		'wid':id,
-		'time':time,
-		'text':text,
-		'uid':uid,
-		'username':username,
-		'following':following,
-		'followed':followed,
-		'gender':gender
-	}
-
+                                                                                                                                                                                                    
 
 if __name__ == "__main__":
     url = "https://m.weibo.cn/comments/hotflow?id=4610404497493299&mid=4610404497493299&max_id_type=0"
-    CatchData(url) #评论爬取 通过，楼中楼爬取未通过
-    id=4610404497493299 #话题id
+    # 话题id,https://m.weibo.cn/detail/4610404497493299
+    # 用户提供detail之后的id即可
+    id=4610404497493299
+    # 第一页的max_id为0
+    CatchData(id,0) #评论爬取 通过，楼中楼爬取未通过
     #getTopic(str(id)) #爬取话题通过
---
+++ readme.md
@@ -0,0 +1,7 @@
+# URL分析
+
+url="https://m.weibo.cn/comments/hotflow?id=4282494510984677&mid=4282494510984677&max_id=225416516976479&max_id_type=0"
+
+其中关键的属性是id、mid、max_id、max_id_type
+
+max_id是上一个请求返回的分页参数，max_id_type默认设置为0，
